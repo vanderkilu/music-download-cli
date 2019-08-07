1c23dbcd8ec2483afb4b3a0d29dbef9aaeb5f964
vanderkilu <vanderkiliku@gmail.com>
2019-08-07 00:44:17 +0000

    implement scraper for links

--- scraper.js
+++ scraper.js
@@ -2,9 +2,16 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 module.exports = async (searchUrl) => {
-    const html = await axios.get(searchUrl)
-    const linkElements = cheerio('.entry > a').map(linkElement => {
+    const {data} = await axios.get(searchUrl)
+    const linkElements = cheerio('h2.entry-title a', data).map(async (i,linkElement) => {
         const link = linkElement.attribs.href
-        return link
-    })
+        const innerHTML = await axios.get(link) 
+        const downloadLink = cheerio('.wp-audio-shortcode a', data).attr('href')
+        const title = cheerio('h1.entry-title', innerHTML.data).text()
+        return  {
+            downloadLink,
+            title
+        }
+    }).get()
+    return Promise.all(linkElements)
 }
