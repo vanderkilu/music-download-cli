2af116c21a8e705867fef662883bedf5adffe1af
vanderkilu <vanderkiliku@gmail.com>
2019-08-07 11:51:51 +0000

    refactor code

--- scraper.js
+++ scraper.js
@@ -1,17 +1,29 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-module.exports = async (searchUrl) => {
+const getSongs = async (searchUrl)=> {
     const {data} = await axios.get(searchUrl)
     const linkElements = cheerio('h2.entry-title a', data).map(async (i,linkElement) => {
         const link = linkElement.attribs.href
-        const innerHTML = await axios.get(link) 
-        const downloadLink = cheerio('.wp-audio-shortcode a',  innerHTML.data).attr('href')
-        const title = cheerio('h1.entry-title', innerHTML.data).text()
+        const songTitle = cheerio(linkElement).text()
         return  {
-            downloadLink,
-            title
+            link,
+            songTitle
         }
     }).get()
-    return Promise.all(linkElements)
+}
+
+const getSongDownloadLink = async (songLink) => {
+    const innerHTML = await axios.get(link) 
+    const downloadLink = cheerio('.wp-audio-shortcode a',  innerHTML.data).attr('href')
+    const title = cheerio('h1.entry-title', innerHTML.data).text()
+    return  {
+        downloadLink,
+        title
+    }
+}
+
+module.exports = {
+    getSongs,
+    getSongDownloadLink
 }
