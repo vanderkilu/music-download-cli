61d85cfd152d7bb8291d87180d63de4a202c13d6
vanderkilu <vanderkiliku@gmail.com>
2019-08-08 19:05:46 +0000

    add loading spinner animation

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const path = require('path')
 const request = require('request')
 const progress = require('request-progress')
 const ProgressBar = require('./progressBar')
+const Spinner = require('cli-spinner').Spinner
 
 const homePage = 'https://www.hitxgh.com/'
+
 
 async function fetchSongs() {
     const questions = []
@@ -31,11 +33,20 @@ async function fetchSongs() {
 }
 
 async function downloadSong() {
-    console.log('fetching songs ......' + '\n')
-    const song = await fetchSongs(homePage)
-    console.log('preparing to download ......'+ '\n')
-    const {downloadLink, title }= await getSong(song.link)
 
+    const spinner1 = new Spinner('retrieving lastest song.. %s')
+    spinner1.setSpinnerString('|/-\\')
+    spinner1.start()
+
+    const song = await fetchSongs(homePage)
+    spinner1.stop()
+
+    const spinner2 = new Spinner('retrieving lastest song.. %s')
+    spinner2.setSpinnerString('|/-\\')
+    spinner2.start()
+
+    const {downloadLink, title }= await getSong(song.link)
+    spinner2.stop()
 
     const pathUrl = path.resolve(__dirname, 'songs', title +'.mp3')
     const writer = fs.createWriteStream(pathUrl)
